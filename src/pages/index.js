016d6112eb35588e7b2a979857f780aa090ec835
Amberley Romo <amberleyjohanna@gmail.com>
2020-09-14 12:17:03 -0500

    title change

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,12 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+// whoop
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Hello howdy</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
